d817d2f0648784aee2da90a9252c9b8fc6de8d07
Jonas Hendrikson <jonas@performa630.local>
2023-06-14 15:01:46 +0200

    somewhat working version

--- chattarina.py
+++ chattarina.py
@@ -7,13 +7,34 @@ from dotenv import dotenv_values
 env_variables = dotenv_values(".env")
 openai.api_key = env_variables.get("OPENAI_API_KEY")
 
-response = openai.Completion.create(
-  model="text-davinci-003",
-  prompt="Ask me something",
-  temperature=0,
-  max_tokens=100,
-  top_p=1,
-  frequency_penalty=0.0,
-  presence_penalty=0.0,
-  stop=["\n"]
-)
+#response = openai.Completion.create(
+#  model="text-davinci-003",
+#  prompt="Ask me something",
+#  temperature=0,
+#  max_tokens=100,
+#  top_p=1,
+#  frequency_penalty=0.0,
+#  presence_penalty=0.0,
+#  stop=["\n"]
+#)
+#
+#models = openai.Model.list()
+#print(models)
+
+while True:
+  print("\033[1m")
+  question = input("User: ")
+  response = openai.ChatCompletion.create(
+#    model="gpt-3.5-turbo",
+    model="gpt-4",
+    temperature=2,
+    messages=[
+      {"role": "system", "content": "You are a helpful assistant. Experienced in linux, macos and general UNIX. Also very hot and flirty."},
+      {"role": "user", "content": question} 
+    ]
+  )
+  print("\033[0m", end="")
+
+  answer = response["choices"][0]["message"]["content"]
+  print("\n\033[1mChattarina:\033[0m ", end="")
+  print(answer)
