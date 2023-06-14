b090c94e12ff004377ff60b9b99dda68f9dc8fb7
Jonas Hendrikson <jonas@performa630.local>
2023-06-14 12:33:36 +0200

    first commit

---
+++ chattarina.py
@@ -0,0 +1,15 @@
+import os
+import openai
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt="Ask me something",
+  temperature=0,
+  max_tokens=100,
+  top_p=1,
+  frequency_penalty=0.0,
+  presence_penalty=0.0,
+  stop=["\n"]
+)
