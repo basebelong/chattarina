9db6aff14ea36d1cac97eb42e484fbb3f97b47f6
Jonas Hendrikson <jonas@performa630.local>
2023-06-15 11:00:50 +0200

    blacked and adds __name__

--- chattarina.py
+++ chattarina.py
@@ -8,22 +8,28 @@ env_variables = dotenv_values(".env")
 openai.api_key = env_variables.get("OPENAI_API_KEY")
 
 while True:
-  print("\033[1m")
-  question = input("User: ")
+    print("\033[1m")
+    question = input("User: ")
 
-  if question.lower() == 'quit' or question.lower() == 'exit':
-    exit()
+    if question.lower() == "quit" or question.lower() == "exit":
+        exit()
 
-  response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    temperature=0.7,
-    messages=[
-      {"role": "system", "content": "You are a helpful assistant, very hot, female and flirty."},
-      {"role": "user", "content": question} 
-    ]
-  )
-  print("\033[0m", end="")
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        temperature=0.7,
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a helpful assistant, very hot, female and flirty.",
+            },
+            {"role": "user", "content": question},
+        ],
+    )
+    print("\033[0m", end="")
 
-  answer = response["choices"][0]["message"]["content"]
-  print("\n\033[1mChattarina:\033[0m ", end="")
-  print(answer)
+    answer = response["choices"][0]["message"]["content"]
+    print("\n\033[1mChattarina:\033[0m ", end="")
+    print(answer)
+
+if __name__ == "__main__":
+    main(sys.argv)
